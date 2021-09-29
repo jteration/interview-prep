afa0d5dd1a91cb4973007485e3cd2e5986d0223f
Jacob Sullivan <jay@jaysull.dev>
2021-09-29 17:31:20 -0500

    Big O analysis of bubble sort

--- implementation-practice/jay/Sort/bubble.mjs
+++ implementation-practice/jay/Sort/bubble.mjs
@@ -5,19 +5,23 @@ function swap(arr, i, j) {
 }
 
 export function bubbleSort(arr) {
-	let swaps = 1;
+	let swaps = 1; // O(1)
 
-	while (swaps > 0) {
-		swaps = 0;
+	while (swaps > 0) { // O(n)
+		swaps = 0; // O(1)
 
-		for (let i = 1; i < arr.length; i += 1) {
-			const leftItem = arr[i - 1];
-			const rightItem = arr[i];
+		for (let i = 1; i < arr.length; i += 1) {  // O(n)
+			const leftItem = arr[i - 1]; // O(1)
+			const rightItem = arr[i]; // O(1)
 
-			if (leftItem > rightItem) {
-				swap(arr, i - 1, i);
-				swaps += 1;
+			if (leftItem > rightItem) { // O(1)
+				swap(arr, i - 1, i); // O(3)
+				swaps += 1; // O(1)
 			}
 		}
 	}
 }
+
+// O(1 + n(1 + n(7)))
+// O(1 + n + 7n^2)
+// O(n^2)
